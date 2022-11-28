4c45d930915f26a2ff632bf2273dd306b956aeed
katherinem23 <113661747+katherinem23@users.noreply.github.com>
2022-11-28 12:51:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ let allMovies = [];
 
 //Define a movie class with parameters title (string), rating (number) and haveWatched (boolean)
 class Movie {
-
+  constructor(title, rating, haveWatched) {
+    this.title = title;
+    this.rating = rating;
+    this.haveWatched = haveWatched;
+  }
 }
 
 //add a movie OBJECT to the allMovies array
-let addMovie = (movie) => {
-
+let addMovie = movie => {
+  allMovies.push(movie);
+  console.log('A new movie is added');
 }
 
 //iterate through all elements of allMovies array
@@ -15,56 +20,74 @@ let addMovie = (movie) => {
 //print out the total number of movies in allMovies array
 let printMovies = () => {
 
+  console.log('Printing all movies...');
+  for (let i = 0; i < allMovies.length; i++) {
+    console.log(
+      allMovies[i].title +
+        ', rating of ' +
+        allMovies[i].rating +
+        ', havewatched: ' +
+        allMovies[i].haveWatched);   
+  }
+  console.log('\nYou have ' + allMovies.length + ' movies in total');
 }
-
 
 //print out to console, only the movies that has a rating higher than rating(argument)
 //print out the total number of matches
-let highRatings = (rating) => {
-
+let highRatings = rating => {
+  console.log('printing movie that has a rating higher than ' + rating);
+  let movieCount = 0;
+  for (let i = 0; i < allMovies.length; i++) {
+    if (allMovies[i].rating > rating) {
+      movieCount++;
+      console.log(
+        allMovies[i].title + ' has a rating of ' + allMovies[i].rating
+      );
+    }
+  }
+  console.log('\nIn total, there are ' + movieCount + ' matches');
 }
 
-
-//Toggle the 'haveWatched' property of the specified movie 
-let changeWatched = (title) => {
-
+//Toggle the 'haveWatched' property of the specified movie
+let changeWatched = title => {
+  for (let i = 0; i < allMovies.length; i++) {
+    if (allMovies[i].title == title) {
+      allMovies[i].haveWatched = !allMovies[i].haveWatched;
+      console.log('Changing the status of the movie...')
+      break;
+    }
+  }
 }
-
-
 
 ////////////////////////////////////////////////////////////
 //Test code - DO NOT DELETE OR EDIT
-let x = new Movie("Spiderman", 3, true);
-let y = new Movie("Citizen Kane", 4, false);
-let z = new Movie("Zootopia", 4.5, true);
+let x = new Movie('Spiderman', 3, true);
+let y = new Movie('Citizen Kane', 4, false);
+let z = new Movie('Zootopia', 4.5, true);
 
-allMovies.push(x,y,z);
+allMovies.push(x, y, z);
 
-console.log("----------------");
-console.log("running program......");
-console.log("----------------");
+console.log('----------------');
+console.log('running program......');
+console.log('----------------');
 printMovies();
 
+let movie1 = new Movie('Parasite', 2, false);
 
-let movie1 = new Movie("Parasite", 2, false);
-
-
-console.log("----------------");
+console.log('----------------');
 addMovie(movie1);
-console.log("----------------");
+console.log('----------------');
 
-
-
-changeWatched("Spiderman");
-console.log("----------------");
+changeWatched('Spiderman');
+console.log('----------------');
 
 printMovies();
-console.log("----------------");
+console.log('----------------');
 
-changeWatched("Spiderman");
-console.log("----------------");
+changeWatched('Spiderman');
+console.log('----------------');
 
 printMovies();
-console.log("----------------");
+console.log('----------------');
 
 highRatings(3.5);
